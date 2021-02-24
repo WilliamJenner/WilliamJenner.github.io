bb83eec5a90cc3c2417f7d32f8eb215218410477
harrygillingham99 <harrygillingham99@gmail.com>
2021-02-24 18:06:29 +0000

    feat(deploy-script): added a custom deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,46 @@
+const ghpages = require("gh-pages");
+const chalk = require("chalk");
+const packageJson = require("../package.json");
+
+const trim = (str) => str.replace(/^\s+|\s+$/gm, "");
+
+const output = {
+  starting: function () {
+    const text = trim(`
+          =========================
+          Publishing GitHub Page
+          =========================
+          > Publishing to: ${packageJson.homepage}...
+      `);
+
+    console.log(chalk.green(text));
+  },
+  error: function (ex) {
+    if (typeof ex !== "object") {
+      console.log(chalk.red(ex));
+      throw new Error(ex);
+    }
+
+    console.log(chalk.red(ex.message));
+    throw ex;
+  },
+  success: function (msg) {
+    console.log(chalk.green(msg));
+  },
+};
+
+output.starting();
+
+var buildDir = __dirname.replace("scripts", "build");
+
+ghpages.publish(
+  buildDir,
+  { add: true, message: "gh-pages deploying site", branch: "master" },
+  (err) => {
+    if (err) {
+      output.error(err);
+    } else {
+      output.success("Successfully published GitHub Page.");
+    }
+  }
+);
